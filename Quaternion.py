4557a8460092811a34821fa08f37eb17dd7fa5f7
William T Jones <wtravisjones@gmail.com>
2013-01-27 09:21:42 -0600

    Code cleanup from lint feedback.

--- Quaternion.py
+++ Quaternion.py
@@ -1,48 +1,53 @@
-import copy, math
+import copy
+import math
 from Vector import Vector
+
 
 class Quaternion(object):
     def __init__(self, *args):
         if len(args) == 0:
             self.scalar = 0.0
             self.vector = Vector(0.0, 0.0, 0.0)
-        else:            
+        else:
             self.scalar = args[0]
             if args[1] is Vector:
                 self.vector = copy.deepcopy(args[1])
             else:
                 self.vector = Vector(args[1], args[2], args[3])
-        
+
     # Properties: W, X, Y, Z
     def __GetW(self):
         return self.scalar
-    
+
     def __SetW(self, value):
         self.scalar = value
-        
+
     w = property(fget=__GetW, fset=__SetW, doc="what?")
-    
+
     def __GetX(self):
-       return self.vector.x
+        return self.vector.x
+
     def __SetX(self, value):
         self.vector.x = value
-        
+
     x = property(fget=__GetX, fset=__SetX, doc="what?")
-     
+
     def __GetY(self):
         return self.vector.y
+
     def __SetY(self, value):
         self.vector.y = value
-        
+
     y = property(fget=__GetY, fset=__SetY, doc="what?")
-    
+
     def __GetZ(self):
         return self.vector.z
+
     def __SetZ(self, value):
         self.vector.z = value
-        
+
     z = property(fget=__GetZ, fset=__SetZ, doc="what?")
-    
+
     def __mul__(self, other):
         if type(other) is not Quaternion:
             raise Exception("Unsupported Operation")
@@ -53,15 +58,15 @@ class Quaternion(object):
             result.y = self.w * other.y + self.y * other.w + self.z * other.x - self.x * other.z
             result.z = self.w * other.z + self.z * other.w + self.x * other.y - self.y * other.x
             return result
-        
+
     def Length(self):
         return math.sqrt(self.w * self.w + self.x * self.x + self.y * self.y + self.z * self.z)
-    
+
     def out(self):
         print "W = " + str(self.scalar) + ", Vector = " + str(self.vector)
-        
+
     def Normalized(self):
-        
+
         if self.Length() == 0:
             return Quaternion(1.0, 0.0, 0.0, 0.0)
         else:
@@ -71,15 +76,15 @@ class Quaternion(object):
             temp.x /= length
             temp.y /= length
             temp.z /= length
-    
+
             return temp
-    
+
     def Normalize(self):
         self = self.Normalized()
-        
+
     def Conjugate(self):
         return Quaternion(self.w, -self.x, -self.y, -self.z)
-        
+
 if __name__ == "__main__":
     p1 = Quaternion(0.0, 1.0, 0.0, 0.0)
     q = Quaternion(.7071, 0.0, 0.0, 0.7071)
@@ -88,7 +93,3 @@ if __name__ == "__main__":
     p2 = q * p1 * q.Conjugate()
     p2.out()
     print str([4.0, 2.1])
-    
-    
-    
-    
--- Vector.py
+++ Vector.py
@@ -1,4 +1,6 @@
-import math, copy
+import math
+import copy
+
 
 class Vector(list):
     """ Vector.py
@@ -11,69 +13,68 @@ class Vector(list):
             - Has properties x, y and z for ease of use
             - Allows for Vector add/mul with a scalar or another Vector
     """
-    def __init__(self, *vec): 
-         if len(vec) == 0:
-             #return Vector([0.0, 0.0, 0.0])
-             super(Vector,self).__init__([0.0, 0.0, 0.0])
-         else:
-             if type(vec[0]) is list:
-                 super(Vector,self).__init__(vec[0]) 
-             else:
-                 if type(vec) is tuple:
-                     super(Vector,self).__init__([i for i in vec])
-             
+    def __init__(self, *vec):
+        if len(vec) == 0:
+            super(Vector, self).__init__([0.0, 0.0, 0.0])
+        else:
+            if type(vec[0]) is list:
+                super(Vector, self).__init__(vec[0])
+            else:
+                if type(vec) is tuple:
+                    super(Vector, self).__init__([i for i in vec])
+
     def __add__(self, other):
         if type(other) is list or type(other) is Vector:
-            return Vector(map(lambda x,y: x+y, self, other))
+            return Vector(map(lambda x, y: x + y, self, other))
         else:
             return Vector([self[i] + other for i in range(len(self))])
-     
+
     def __sub__(self, other):
         if type(other) is list or type(other) is Vector:
-            return Vector(map(lambda x,y: x-y, self, other))
+            return Vector(map(lambda x, y: x - y, self, other))
         else:
-            return Vector([self[i] + other for i in range(len(self))]) 
-        
+            return Vector([self[i] + other for i in range(len(self))])
+
     def __mul__(self, other):
         if type(other) is list or type(other) is Vector:
-             return Vector(map(lambda x,y: x*y, self, other))
+            return Vector(map(lambda x, y: x * y, self, other))
         else:
-             return Vector([self[i] * other for i in range(len(self))])
+            return Vector([self[i] * other for i in range(len(self))])
 
     # Properties of X, Y, Z for convenience
     def __GetX(self):
-         return self[0]
-     
+        return self[0]
+
     def __SetX(self, value):
         self[0] = value
-        
+
     x = property(fget=__GetX, fset=__SetX, doc="what?")
-     
+
     def __GetY(self):
         return self[1]
-    
+
     def __SetY(self, value):
         self[1] = value
-        
+
     y = property(fget=__GetY, fset=__SetY, doc="what?")
-    
+
     def __GetZ(self):
         return self[2]
-    
+
     def __SetZ(self, value):
         self[2] = value
-        
+
     z = property(fget=__GetZ, fset=__SetZ, doc="what?")
-    
+
     def out(self):
         print self
-        
+
     def Normalized(self):
         if len(self) > 3 or len(self) < 2:
-            raise Exception ("Normalization not supported on this Vector")
-        
+            raise Exception("Normalization not supported on this Vector")
+
         temp = copy.deepcopy(self)
-        
+
         if len(self) == 2:
             length = math.sqrt(temp[0] * temp[0] + temp[1] * temp[1])
             temp[0] /= length
@@ -83,34 +84,28 @@ class Vector(list):
             temp[0] /= length
             temp[1] /= length
             temp[2] /= length
-        
+
         return temp
-    
+
     def Normalize(self):
-        #self = self.Normalized()
         n = self.Normalized()
         for i in range(len(n)):
             self[i] = n[i]
-        
+
 
 if __name__ == "__main__":
-    
+
     a = Vector()
 
-    a = Vector([2.0,2.0,2.0])
+    a = Vector([2.0, 2.0, 2.0])
     a.x = 4.0
-  
-    b = a * Vector(3,3,3)
-    c = Vector(2.0,2.0)
+
+    b = a * Vector(3, 3, 3)
+    c = Vector(2.0, 2.0)
     d = Vector(9.0, 7.0)
-    dist = d- c
+    dist = d - c
     print dist
-   
+
     dist.Normalize()
     print dist
     print c + dist
-    
-   # print c - Vector(3.0,3.0)
-   # print b.Normalized()
-   
-   
